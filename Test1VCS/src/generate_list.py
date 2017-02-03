c1a757489d1b655fdeebc6dd3b200131ceb01858
Navaphon <kr.navaphon_st@tni.ac.th>
2017-02-03 02:12:46 +0000

    C1-First version

---
+++ Test1VCS/src/generate_list.py
@@ -0,0 +1,19 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.randint(-10,10))]
+    return alist
+
+"""
+prinnt a generated list
+"""
+def printIT():
+    print(generate_ilst())
+    def main():
+        printIt()
+        
+"""
+If this script file is called, it will run main() directly
+"""
+if __name__ == '__main__':
+    print("Test printIt():")
+    main()
